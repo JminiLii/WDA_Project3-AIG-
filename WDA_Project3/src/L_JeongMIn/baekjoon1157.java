73cb20a38409c338c550c01f51b478748a42f3ea
LeeJeongMin <jm3089@g.yju.ac.kr>
2022-05-02 23:13:17 +0900

    JM baekjoon1157 again

---
+++ WDA_Project3/src/L_JeongMIn/baekjoon1157.java
@@ -0,0 +1,29 @@
+package L_JeongMIn;
+import java.util.*;
+
+public class baekjoon1157 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int[] abc = new int[26];
+		
+		String txt = in.next();
+		txt = txt.toUpperCase();
+		for (int i = 0; i < txt.length(); i++) {
+			abc[txt.charAt(i)-'A']++;
+		}
+		int max=0, ans=0;
+		for (int i = 0; i < abc.length; i++) {
+			if(max < abc[i]) {
+				max = abc[i];
+				ans = i;
+			}
+			else if(max == abc[i]) {
+				ans = -2;
+			}
+		}
+		int sum = ans + 65;
+		char ha = (char)sum;
+		System.out.println(ha);
+	}
+}
