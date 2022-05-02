38bb8603093b8a14a6169e5701eb40f9415eaf5c
(junhong) <(hongboy1@g.yju.ac.kr)>
2022-05-02 19:17:26 +0900

    PJH_Add bakJoon

---
+++ WDA_Project3/src/P_JunHong/Num1157.java
@@ -0,0 +1,32 @@
+package P_JunHong;
+
+import java.util.Scanner;
+
+public class Num1157 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		s=s.toUpperCase();
+		int arr[] = new int [26];
+		for(int i=0; i<s.length();i++) {
+			arr[s.charAt(i)-65]++;
+		} 
+
+		int max = -1;
+		char ch = '?';
+ 
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65);
+			}
+			else if (arr[i] == max) {
+				ch = '?';
+			}
+		}
+ 
+		System.out.print(ch);
+		
+	}
+}
---
+++ WDA_Project3/src/P_JunHong/Num1964.java
@@ -0,0 +1,20 @@
+package P_JunHong;
+
+import java.util.Scanner;
+
+public class Num1964 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long num=4;
+		long sum=4;
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt()-1;
+		
+		for(int i=0; i<n;i++) {
+			num+=3;
+			sum+=num;
+		}
+		System.out.println((sum+1)% 45678);
+	}
+
+}
