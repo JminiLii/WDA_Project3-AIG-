6911f1c9911c4a6e18a47874e438286ec69ac807
takyeoung <103091272+takyeoung@users.noreply.github.com>
2022-05-12 15:27:36 +0900

    new test

--- WDA_Project3/src/K_TakYoung/Backjoon1264.java
+++ WDA_Project3/src/K_TakYoung/Backjoon1264.java
@@ -6,12 +6,13 @@ public class Backjoon1264 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        for(;;){
+        for(;;) {
         String str = sc.nextLine();
         int sum = 0;
-        if(str.equals("#")){
+        if(str.equals("#")) {
             break;
         }
+        
         for(int i= 0; i<str.length(); i++) {
             if(str.charAt(i)=='a' || str.charAt(i)=='e'|| str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u')
         
