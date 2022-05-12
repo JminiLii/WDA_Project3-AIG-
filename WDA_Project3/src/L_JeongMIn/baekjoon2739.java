202edcf78808aea6d49307c6ccfacd1875f62406
JminiLii <103085094+JminiLii@users.noreply.github.com>
2022-05-12 15:14:25 +0900

    JMbaekjoon2739

---
+++ WDA_Project3/src/L_JeongMIn/baekjoon2739.java
@@ -0,0 +1,15 @@
+package L_JeongMIn;
+import java.util.Scanner;
+
+public class baekjoon2739 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for (int i = 1; i < 10; i++) {
+			System.out.println(n + " * " + i + " = " + n*i);
+		}
+	}
+}
+
+
