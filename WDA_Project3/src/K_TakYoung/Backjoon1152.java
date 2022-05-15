4a5130ee9293325803f4bd0c9477035f2501c7a0
takyeoung <103091272+takyeoung@users.noreply.github.com>
2022-05-15 16:07:23 +0900

    4주자 5622

--- WDA_Project3/src/K_TakYoung/Backjoon1152.java
+++ WDA_Project3/src/K_TakYoung/Backjoon1152.java
@@ -2,9 +2,8 @@ package K_TakYoung;
 import java.util.Scanner;
 public class Backjoon1152 {
     public static void main(String[] args) {
-        
-    
-    Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
 		int count =1;
 		String word = sc.nextLine();
 		word = word.toUpperCase();
