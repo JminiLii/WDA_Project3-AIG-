e74f8b9d94ab2377142730ccd89fc2ceafe00ad3
takyeoung <103091272+takyeoung@users.noreply.github.com>
2022-06-02 16:43:40 +0900

    Create Backjoon15351.java

---
+++ WDA_Project3/src/K_TakYoung/Backjoon15351.java
@@ -0,0 +1,14 @@
+package K_TakYoung;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjoon15351 {
+    public static void main(String[] args) {
+        BufferedReader br;
+        int count =0;
+        String [] str = null;
+        try {}
+        
+    }
+}
